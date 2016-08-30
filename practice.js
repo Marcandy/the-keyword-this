02176fc2241b40d7c0c845c9bc56edd1d0937ae0
marcandy <marcandy.nj@gmail.com>
2016-08-30 15:13:02 -0500

    added user object

--- practice.js
+++ practice.js
@@ -29,9 +29,15 @@
   //getUsername --> which is a function that returns the current object's username property. *Don't use 'user' instead use the 'this' keyword*
 
     //Code Here
-
+var user = {
+  username: 'Marc',
+  email: 'go@gmail.com',
+  getUsername: function () {
+    return this.username;
+  }
+};
 //Now, invoke the getUsername method and verify you got the username of the object and not anything else.
-
+user.getUsername();
 
 //Next Problem
 
@@ -47,6 +53,7 @@ prius.moveCar(); //increments prius' move property by 10. Returns the new move p
 mustang.moveCar(); //increments mustang' move property by 10. Returns the new move property.
 
 //Hint, you'll need to add a move property and write a moveCar function which is added to every object that is being returned from the Car function. You'll also need to use the 'this' keyword properly in order to make sure you're invoking moveCar on the right object (prius vs mustang).
+
 
 
 
